adf6972dd9102587974ccf86e639c6d21c44c440
kimmsoll <kmgorae@gmail.com>
2025-01-22 00:47:10 +0900

    feat: 로그인 페이지 생성

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+export default function Login() {
+  return (
+    <section className="h-screen bg-gray-50 flex items-center justify-center">
+      <h1 className="whitespace-pre text-center text-lg font-bold">
+        {`손쉽게 기록하고\n나의 변화를 한눈에 파악하세요`}
+      </h1>
+    </section>
+  );
+}
