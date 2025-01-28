4c8a146abf6a7ed2f82833643e9812a44136d082
kimmsoll <kmgorae@gmail.com>
2025-01-29 00:59:35 +0900

    style: Sidebar 스타일 수정

--- src/components/layout/Sidebar/index.tsx
+++ src/components/layout/Sidebar/index.tsx
@@ -17,7 +17,7 @@ export default function Sidebar() {
       <div
         className={`fixed flex flex-col gap-4 top-0 left-0 h-full w-64 bg-white p-5 transition-transform duration-300 ${
           isSidebarOpen ? "translate-x-0" : "-translate-x-full"
-        }`}
+        } shadow-lg`}
       >
         <button
           className="text-black"
@@ -27,19 +27,25 @@ export default function Sidebar() {
           <CloseIcon width={24} />
         </button>
         <nav>
-          <ul className="flex flex-col gap-5 mt-5">
+          <ul className="flex flex-col gap-2 mt-5">
             <li>
-              <a href="/" className="text-black text-lg">
+              <a
+                href="/"
+                className="block text-black text-lg w-full p-3 transition-all hover:border-b hover:border-black"
+              >
                 대시보드
               </a>
             </li>
             <li>
-              <a href="/analytics" className="text-black text-lg">
+              <a
+                href="/analytics"
+                className="block text-black text-lg w-full p-3 transition-all hover:border-b hover:border-black"
+              >
                 분석
               </a>
             </li>
             <li>
-              <button className="text-black text-lg" onClick={logout}>
+              <button className="text-black text-lg p-3" onClick={logout}>
                 로그아웃
               </button>
             </li>
