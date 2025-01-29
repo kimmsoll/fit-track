851236578d7280b4cd40f57c0c9a167815d8a141
kimmsoll <kmgorae@gmail.com>
2025-01-29 16:14:45 +0900

    feat: FormHeader 생성

---
+++ src/app/records/add/page.tsx
@@ -0,0 +1,5 @@
+import FormHeader from "@/components/layout/Header/FormHeader";
+
+export default function RecordsAdd() {
+  return <FormHeader title="운동 기록 추가" />;
+}
---
+++ src/components/layout/Header/FormHeader.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import ArrowBackIcon from "@/components/icons/ArrowBackIcon";
+import { useRouter } from "next/navigation";
+
+interface Props {
+  title: string;
+}
+
+export default function FormHeader({ title }: Props) {
+  const router = useRouter();
+
+  const handleGoBack = () => {
+    router.back();
+  };
+
+  return (
+    <header className="w-full flex items-center justify-center bg-customGray-100 p-2 border-b border-customGray-300 fixed">
+      <button
+        className="absolute left-2 text-customGray-900"
+        onClick={handleGoBack}
+        aria-label="Router Back"
+      >
+        <ArrowBackIcon />
+      </button>
+      <h1 className="font-bold text-center">{title}</h1>
+    </header>
+  );
+}
